b02338ec7c74fa2d8942b5c9e29bd9c21fe066c7
caubekimo <caubekimo@gmail.com>
2022-01-08 11:46:37 +0800

    改 gui

--- Homework2/gui.py
+++ Homework2/gui.py
@@ -10,54 +10,54 @@ from keras.models import load_model
 model = load_model('traffic_classifier.h5')
 
 #dictionary to label all traffic signs class.
-classes = { 1:'Speed limit (20km/h)',
-            2:'Speed limit (30km/h)', 
-            3:'Speed limit (50km/h)', 
-            4:'Speed limit (60km/h)', 
-            5:'Speed limit (70km/h)', 
-            6:'Speed limit (80km/h)', 
-            7:'End of speed limit (80km/h)', 
-            8:'Speed limit (100km/h)', 
-            9:'Speed limit (120km/h)', 
-            10:'No passing', 
-            11:'No passing veh over 3.5 tons', 
-            12:'Right-of-way at intersection', 
-            13:'Priority road', 
-            14:'Yield', 
-            15:'Stop', 
-            16:'No vehicles', 
-            17:'Veh > 3.5 tons prohibited', 
-            18:'No entry', 
-            19:'General caution', 
-            20:'Dangerous curve left', 
-            21:'Dangerous curve right', 
-            22:'Double curve', 
-            23:'Bumpy road', 
-            24:'Slippery road', 
-            25:'Road narrows on the right', 
-            26:'Road work', 
-            27:'Traffic signals', 
-            28:'Pedestrians', 
-            29:'Children crossing', 
-            30:'Bicycles crossing', 
-            31:'Beware of ice/snow',
-            32:'Wild animals crossing', 
-            33:'End speed + passing limits', 
-            34:'Turn right ahead', 
-            35:'Turn left ahead', 
-            36:'Ahead only', 
-            37:'Go straight or right', 
-            38:'Go straight or left', 
-            39:'Keep right', 
-            40:'Keep left', 
-            41:'Roundabout mandatory', 
-            42:'End of no passing', 
-            43:'End no passing veh > 3.5 tons' }
+classes = { 1:'速度限制 (20km/h)',
+            2:'速度限制 (30km/h)', 
+            3:'速度限制 (50km/h)', 
+            4:'速度限制 (60km/h)', 
+            5:'速度限制 (70km/h)', 
+            6:'速度限制 (80km/h)', 
+            7:'速限解除 (80km/h)', 
+            8:'速度限制 (100km/h)', 
+            9:'速度限制 (120km/h)', 
+            10:'禁止超車', 
+            11:'3.5 噸以上車輛禁止超車', 
+            12:'優先行駛(下一個路口有優先行駛權)', 
+            13:'幹道先行', 
+            14:'讓路', 
+            15:'停止', 
+            16:'道路封閉', 
+            17:'3.5 噸以上車輛禁止進入', 
+            18:'禁止所有車輛進入', 
+            19:'危險', 
+            20:'左彎', 
+            21:'右彎', 
+            22:'連續彎路先向左', 
+            23:'道路顛簸', 
+            24:'路面溼滑', 
+            25:'右側車道縮減', 
+            26:'道路施工', 
+            27:'注意號誌', 
+            28:'當心行人', 
+            29:'當心兒童', 
+            30:'當心腳踏車', 
+            31:'當心路面結冰/積雪',
+            32:'注意野生動物', 
+            33:'取消車速與超車限制', 
+            34:'前方車道僅准右轉', 
+            35:'前方車道僅准左轉', 
+            36:'前方車道僅准直行', 
+            37:'前方車道僅准直行及右轉', 
+            38:'前方車道僅准直行及左轉', 
+            39:'靠右行駛', 
+            40:'靠左行駛', 
+            41:'圓環遵行方向', 
+            42:'恢復車輛可超車', 
+            43:'恢復 3.5 噸以上車輛可超車' }
 
 #initialise GUI
 top=tk.Tk()
 top.geometry('800x600')
-top.title('Traffic sign classification')
+top.title('交通號誌辨識')
 top.configure(background='#CDCDCD')
 
 label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
@@ -77,8 +77,8 @@ def classify(file_path):
     label.configure(foreground='#011638', text=sign) 
 
 def show_classify_button(file_path):
-    classify_b=Button(top,text="Classify Image",command=lambda: classify(file_path),padx=10,pady=5)
-    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+    classify_b=Button(top,text="辨識",command=lambda: classify(file_path),padx=25,pady=25)
+    classify_b.configure(background='#364156', foreground='white',font=('arial',16,'bold'))
     classify_b.place(relx=0.79,rely=0.46)
 
 def upload_image():
@@ -95,13 +95,13 @@ def upload_image():
     except:
         pass
 
-upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
-upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+upload=Button(top,text="選擇圖片",command=upload_image,padx=150,pady=10)
+upload.configure(background='#364156', foreground='white',font=('arial',16,'bold'))
 
 upload.pack(side=BOTTOM,pady=50)
 sign_image.pack(side=BOTTOM,expand=True)
 label.pack(side=BOTTOM,expand=True)
-heading = Label(top, text="Know Your Traffic Sign",pady=20, font=('arial',20,'bold'))
+heading = Label(top, text="交通號誌辨識",pady=20, font=('arial',20,'bold'))
 heading.configure(background='#CDCDCD',foreground='#364156')
 heading.pack()
 top.mainloop()
